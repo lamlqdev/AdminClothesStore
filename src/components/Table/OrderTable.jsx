f7d02b4ad436a31b0b588ac9a68f7f305d077a50
Le Quang Lam <work.lamsun@gmail.com>
2024-12-03 12:59:16 +0700

    loc danh sách order, cai thien hien thi

--- src/components/Table/OrderTable.jsx
+++ src/components/Table/OrderTable.jsx
@@ -10,12 +10,12 @@ import {
   Typography,
   Button,
   CardBody,
-  Chip,
   CardFooter,
   Avatar,
 } from "@material-tailwind/react";
 
 const TABLE_HEAD = [
+  "Number",
   "Order ID",
   "Order Time",
   "Customer",
@@ -26,16 +26,47 @@ const TABLE_HEAD = [
 
 export default function OrderTable({ orders }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [statusFilter, setStatusFilter] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredOrders = orders.filter(
-    (order) =>
-      order.id.toString().includes(searchTerm) ||
-      order.userInfo.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const handleStatusChange = (event) => {
+    setStatusFilter(event.target.value);
+  };
+
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(event.target.value);
+  };
+
+  const sortedOrders = [...orders].sort(
+    (a, b) => new Date(b.orderTime) - new Date(a.orderTime)
   );
+
+  const filteredOrders = sortedOrders
+    .filter((order) => {
+      // Chuyển đổi Firebase Timestamp thành đối tượng Date để so sánh
+      const orderDate = new Date(order.orderTime.seconds * 1000); // Firebase Timestamp (seconds) -> Date
+      const isWithinDateRange =
+        (!startDate || orderDate >= new Date(startDate)) &&
+        (!endDate || orderDate <= new Date(endDate));
+      return (
+        (order.id.toString().includes(searchTerm) ||
+          order.userInfo.name
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())) &&
+        (statusFilter === "" || order.orderStatus === statusFilter) &&
+        isWithinDateRange
+      );
+    })
+    .reverse();
 
   return (
     <Card className="h-full w-full">
@@ -48,7 +79,18 @@ export default function OrderTable({ orders }) {
           </div>
         </div>
         <div className="flex flex-col my-4 items-center justify-between gap-4 md:flex-row">
-          <div className="flex gap-2 ml-3"></div>
+          <div className="flex gap-2 ml-3">
+            <select
+              value={statusFilter}
+              onChange={handleStatusChange}
+              className="border p-2 border-gray-300 rounded-lg shadow-sm pl-4 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
+            >
+              <option value="">All</option>
+              <option value="Completed">Completed</option>
+              <option value="Waiting for payment">Waiting for payment</option>
+              <option value="Active">Active</option>
+            </select>
+          </div>
           <div className="w-full md:w-72 mr-4">
             <div className="relative w-full">
               <Input
@@ -59,6 +101,23 @@ export default function OrderTable({ orders }) {
               />
               <MagnifyingGlassIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500" />
             </div>
+          </div>
+          {/* Date Range Picker */}
+          <div className="flex gap-4">
+            <Input
+              type="date"
+              value={startDate}
+              onChange={handleStartDateChange}
+              placeholder="Start Date"
+              className="border border-gray-300 rounded-lg shadow-sm pl-4 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
+            />
+            <Input
+              type="date"
+              value={endDate}
+              onChange={handleEndDateChange}
+              placeholder="End Date"
+              className="border border-gray-300 rounded-lg shadow-sm pl-4 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
+            />
           </div>
         </div>
       </CardHeader>
@@ -83,11 +142,20 @@ export default function OrderTable({ orders }) {
             </tr>
           </thead>
           <tbody>
-            {filteredOrders.map((order) => {
+            {filteredOrders.map((order, index) => {
               const classes = "p-4 border-b border-blue-gray-50";
 
               return (
                 <tr key={order.id}>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {index + 1}
+                    </Typography>
+                  </td>
                   <td className={classes}>
                     <Typography
                       variant="small"
@@ -140,22 +208,23 @@ export default function OrderTable({ orders }) {
                       color="blue-gray"
                       className="font-normal"
                     >
-                      {order.total} $
+                      {parseFloat(order.total).toFixed(2)} $
                     </Typography>
                   </td>
                   <td className={classes}>
-                    <Chip
-                      variant="ghost"
-                      size="sm"
-                      value={order.orderStatus}
-                      color={
-                        order.orderStatus === "Delivered"
-                          ? "green"
-                          : order.orderStatus === "Pending"
-                          ? "yellow"
-                          : "red"
-                      }
-                    />
+                    <span
+                      className={`px-3 py-1 rounded-full text-xs font-semibold ${
+                        order.orderStatus === "Completed"
+                          ? "bg-green-100 text-green-800"
+                          : order.orderStatus === "Waiting for payment"
+                          ? "bg-yellow-100 text-yellow-800"
+                          : order.orderStatus === "Active"
+                          ? "bg-blue-100 text-blue-800"
+                          : "bg-red-100 text-red-800"
+                      }`}
+                    >
+                      {order.orderStatus}
+                    </span>
                   </td>
                   <td className={classes}>
                     <Link to={`/order/${order.id}`}>
@@ -174,19 +243,6 @@ export default function OrderTable({ orders }) {
           </tbody>
         </table>
       </CardBody>
-      <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-        <Typography variant="small" color="blue-gray" className="font-normal">
-          Page 1 of 10
-        </Typography>
-        <div className="flex gap-2">
-          <Button variant="outlined" size="sm">
-            Previous
-          </Button>
-          <Button variant="outlined" size="sm">
-            Next
-          </Button>
-        </div>
-      </CardFooter>
     </Card>
   );
 }
